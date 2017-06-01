82111fbab793f6dc000c6846049573e093b6d3d8
r-richards <rear-view9>
2017-06-01 19:10:07 +0100

    Update 1st June

--- src/MainApplication.java
+++ src/MainApplication.java
@@ -43,12 +43,13 @@ class MainApplication {
         String message = input.nextLine();
         System.out.println(CryptMessage(key, message));
     }
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("Message Encryption System 3.0");
         System.out.println("Please select an option:");
         System.out.println("[1] - Encrypt Message");
         System.out.println("[2] - Decrypt Message");
         System.out.println("[3] - View Current Key");
+        System.out.println("[4] - Exit");
         String opt = input.nextLine();
         if(Objects.equals(opt, "1")){
             encrypt();
@@ -59,8 +60,12 @@ class MainApplication {
         else if(Objects.equals(opt, "3")){
             System.out.println("The current key setting is: "+setting);
         }
+        else if(Objects.equals(opt, "4")){
+            System.exit(0);
+        }
         else{
             System.out.println("Invalid input.");
+            MainApplication.main(new String[]{});
         }
     }
 }
